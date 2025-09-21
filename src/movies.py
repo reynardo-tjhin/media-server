08123492ef51162843af9ec860aabd0df42f3e1b
Reynardo Tjhin <reychen26@gmail.com>
2025-09-22 00:09:37 +1000

    added footer and filter genre UI without backend

--- src/movies.py
+++ src/movies.py
@@ -5,7 +5,7 @@ from src.db import get_db
 
 bp = Blueprint('movies', __name__, url_prefix='/movies')
 
-@bp.route("/", methods=["GET", "POST"])
+@bp.route("/", methods=["GET"])
 def home():
 
     # search is not empty
@@ -16,31 +16,37 @@ def home():
     # get the movies
     db = get_db()
     movies = db.execute(
-        'SELECT m.poster_location, m.name, m.description, '
+        'SELECT m.id, m.poster_location, m.name, '
         '       m.imdb_rating, m.rotten_tomatoes_rating, m.metacritic_rating, '
-        # '       strftime("%e", m.release_date) || " " || '
-        # '       CASE strftime("%m", m.release_date) '
-        # '         WHEN "01" THEN "January" '
-        # '         WHEN "02" THEN "February" '
-        # '         WHEN "03" THEN "March" '
-        # '         WHEN "04" THEN "April" '
-        # '         WHEN "05" THEN "May" '
-        # '         WHEN "06" THEN "June" '
-        # '         WHEN "07" THEN "July" '
-        # '         WHEN "08" THEN "August" '
-        # '         WHEN "09" THEN "September" '
-        # '         WHEN "10" THEN "October" '
-        # '         WHEN "11" THEN "November" '
-        # '         WHEN "12" THEN "December" '
-        # '       END || " " || '
         '       strftime("%Y", m.release_date) AS release_year, '
         '       GROUP_CONCAT(g.name, ", ") AS genres '
         'FROM movie AS m '
         '  LEFT JOIN movie_genre AS mg ON m.id = mg.movie_id '
         '  LEFT JOIN genre AS g ON mg.genre_id = g.id '
         'WHERE m.name LIKE ? '
-        'GROUP BY m.name;', ('%'+movie_name+'%',)
+        'GROUP BY m.id;', ('%'+movie_name+'%',)
     ).fetchall()
 
+    # filter by genre
+    genres = db.execute('SELECT * FROM genre;').fetchall()
+    genres_selected = []
+    genres_final = []
+    for genre in genres:
+        if genre[1] in request.args.getlist('genre'):
+            genres_final.append({
+                'id': genre[0],
+                'name': genre[1],
+                'checked': 'checked',
+            })
+            genres_selected.append(genre[1])
+        else:
+            genres_final.append({
+                'id': genre[0],
+                'name': genre[1],
+                'checked': '',
+            })
+
     return render_template("movies/home.jinja2",
-                           movies=movies,)
+                           movies=movies,
+                           genres=genres_final,
+                           genres_selected=genres_selected,)
