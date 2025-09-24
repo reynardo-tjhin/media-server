25649179629755e26ec2201de73af1b39bf97cba
Reynardo Tjhin <reychen26@gmail.com>
2025-09-25 00:19:53 +1000

    imdb rating sort and genre improvement

--- src/movies.py
+++ src/movies.py
@@ -8,10 +8,26 @@ bp = Blueprint('movies', __name__, url_prefix='/movies')
 @bp.route("/", methods=["GET"])
 def home():
 
+    print(request.args)
+
     # search is not empty
     movie_name = ""
     if (request.args.get("movie-name")):
         movie_name = request.args.get("movie-name")
+
+    # dictionary of imdb_ratings
+    imdb_ratings = {
+        "Show All": -1,
+        "8+ (Excellent)": 8,
+        "7+ (Good)": 7,
+        "6+ (Fair)": 6,
+        "5+ (Average)": 5,
+    }
+    imdb_rating = -1
+    imdb_rating_text = "Any IMDb Rating"
+    if (request.args.get("imdb_rating")):
+        imdb_rating = imdb_ratings.get(request.args.get("imdb_rating"))
+        imdb_rating_text = request.args.get("imdb_rating")
 
     # get the db
     db = get_db()
@@ -52,13 +68,13 @@ def home():
             f'    LEFT JOIN movie_genre AS mg ON m.id = mg.movie_id '
             f'    LEFT JOIN genre AS g ON mg.genre_id = g.id '
             f'  WHERE g.id IN ({genre_id_query})'
+            f'        AND m.imdb_rating > ? '
             f'  GROUP BY m.id '
             f'  HAVING COUNT(DISTINCT g.id) = ?'
             f') AS c '
             f'  LEFT JOIN movie_genre AS mg ON c.id = mg.movie_id '
             f'  LEFT JOIN genre AS g ON g.id = mg.genre_id '
-            f'GROUP BY c.id '
-            , (*valid_genre_ids, len(valid_genre_ids),)
+            f'GROUP BY c.id ', (*valid_genre_ids, imdb_rating, len(valid_genre_ids),)
         ).fetchall()
 
     # get the movies normally
@@ -72,11 +88,13 @@ def home():
             '  LEFT JOIN movie_genre AS mg ON m.id = mg.movie_id '
             '  LEFT JOIN genre AS g ON mg.genre_id = g.id '
             'WHERE m.name LIKE ? '
-            'GROUP BY m.id;', ('%'+movie_name+'%',)
+            '      AND m.imdb_rating > ? '
+            'GROUP BY m.id;', ('%'+movie_name+'%', imdb_rating,)
         ).fetchall()
 
     return render_template("movies/home.jinja2",
                            movie_name=movie_name,
                            movies=movies,
                            genres=genres_final,
-                           genres_selected=genres_selected,)
+                           genres_selected=genres_selected,
+                           imdb_rating_text=imdb_rating_text)
