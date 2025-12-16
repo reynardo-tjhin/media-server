f145afe37dce32ab22cfef730fb5c477467b610b
Reynardo Tjhin <reychen26@gmail.com>
2025-12-16 23:32:41 +1100

    update UI for delete and edit movies and udpate backend for delete movie

--- src/admin.py
+++ src/admin.py
@@ -90,70 +90,6 @@ def movies():
 
 
 
-def _validate_movie_entries(movie_data: dict[str, Any]) -> str:
-    
-    # initialise the variable 'error' (to be returned)
-    error = ""
-    
-    # get the database
-    db = get_db()
-    
-    # get the movie data
-    movie_name = movie_data['movieName']
-    movie_description = movie_data['movieDescription']
-    imdb_rating = movie_data['imdbRating']
-    rotten_tomatoes_rating = movie_data['rottenTomatoesRating']
-    metacritic_rating = movie_data['metacriticRating']
-    release_date = movie_data['releaseDate']
-    media_location = movie_data['mediaLocation']
-    poster_location = movie_data['posterLocation']
-    duration = movie_data['duration']
-    
-    # validation 1: the below data are all required
-    if not movie_name:
-        error = "Movie Name is required."
-    elif not movie_description:
-        error = "Movie Description is required."
-    elif not release_date:
-        error = "Release Date is required."
-    elif not media_location:
-        error = "Media Location is required."
-    elif not poster_location:
-        error = "Poster Location is required."
-
-    # validation 2: no two movie names exists
-    name = db.execute('SELECT name FROM movie WHERE name = ?', (movie_name,)).fetchone()
-    if (name is not None):
-        error = "Movie name already exists."
-    
-    # validation 3: no two media locations is the same
-    loc = db.execute('SELECT media_location FROM movie WHERE media_location = ?', (media_location,)).fetchone()
-    if (loc is not None):
-        error = "Media location already exists. New movie cannot be from the same existing media location."
-
-    # validation 3: both metacritic rating and rotten tomatoes rating need to be from 0 to 100
-    pattern = r'^(100|[1-9]?[0-9])$'
-    if (rotten_tomatoes_rating):
-        if not bool(re.match(pattern, rotten_tomatoes_rating)):
-            error = "Rotten Tomatoes Rating should be from 0 to 100."
-    if (metacritic_rating):
-        if not bool(re.match(pattern, metacritic_rating)):
-            error = "Metacritic Rating should be from 0 to 100."
-
-    # validation 4: imdb rating needs to be from 0.0 to 10.0
-    pattern = r'^(10\.0|[0-9]\.[0-9])$'
-    if (imdb_rating):
-        if not bool(re.match(pattern, imdb_rating)):
-            error = "IMDb Rating should be from 0.0 to 10.0 (strictly only 1 decimal place)."
-
-    if (duration):
-        if (not duration.isnumeric()):
-            error = "Duration needs to be a number"
-
-    return error
-
-
-
 @bp.route('/add-movie', methods=["GET", "POST"])
 @admin_required
 def add_movie():
@@ -211,60 +147,7 @@ def delete_movie(movie_id: str):
 
 
 
-@bp.route('/<string:genre_id>')
-@admin_required
-def genre(genre_id: str):
-    # define columns to show and its style
-    table_columns = [
-        {'key': 'id', 'label': 'ID', 'width': '50px'},
-        {'key': 'name', 'label': 'Name', 'width': '150px'},
-        {'key': 'description', 'label': 'Description', 'width': '300px'},
-        {'key': 'imdb_rating', 'label': 'IMDB', 'width': '80px'},
-        {'key': 'rotten_tomatoes_rating', 'label': 'RT', 'width': '80px'},
-        {'key': 'metacritic_rating', 'label': 'Metacritic', 'width': '80px'},
-        {'key': 'release_date', 'label': 'Release Date', 'width': '100px'},
-        {'key': 'media_location', 'label': 'Media Location', 'width': '200px'},
-        {'key': 'delete', 'label': 'Delete', 'width': '80px'},
-        {'key': 'update', 'label': 'Update', 'width': '80px'},
-    ]
-
-    # get the movies data that have the specific genre
-    db = get_db()
-    genre = db.execute('SELECT * from genre where id = ?', (genre_id,)).fetchone()
-    movies = db.execute(
-        'SELECT m.* '
-        'FROM movie AS m '
-        'JOIN movie_genre AS mg ON m.id = mg.movie_id '
-        'WHERE mg.genre_id = ?', (genre_id,)
-    ).fetchall()
-
-    # no movies shown
-    style = "display: none;"
-    if (len(movies) <= 0):
-        style = "display: block;"
-
-    return render_template("admin/genre.jinja2",
-                           genre=genre,
-                           movies=movies,
-                           table_columns=table_columns,
-                           style=style,)
-
-
-
-@bp.route('/delete-genre/<string:genre_id>', methods=["POST"])
-@admin_required
-def delete_genre(genre_id: str):
-    # returns a 404 if genre_id does not exist
-    get_genre(genre_id)
-
-    # delete the genre from database
-    db = get_db()
-    db.execute('DELETE FROM genre WHERE id = ?', (genre_id,))
-    db.commit()
-
-    return redirect(url_for('admin.movies'))
-
-@bp.route('/edit-movie', methods=["GET", "POST"])
+@bp.route('/edit-movie/<string:movie_id>', methods=["GET", "POST"])
 @admin_required
 def edit_movie():
     """
@@ -393,6 +276,63 @@ def edit_movie():
                            action=request.args['action'],
                            movie_genres=movie_genres,)
 
+
+
+@bp.route('/<string:genre_id>')
+@admin_required
+def genre(genre_id: str):
+    # define columns to show and its style
+    table_columns = [
+        {'key': 'id', 'label': 'ID', 'width': '50px'},
+        {'key': 'name', 'label': 'Name', 'width': '150px'},
+        {'key': 'description', 'label': 'Description', 'width': '300px'},
+        {'key': 'imdb_rating', 'label': 'IMDB', 'width': '80px'},
+        {'key': 'rotten_tomatoes_rating', 'label': 'RT', 'width': '80px'},
+        {'key': 'metacritic_rating', 'label': 'Metacritic', 'width': '80px'},
+        {'key': 'release_date', 'label': 'Release Date', 'width': '100px'},
+        {'key': 'media_location', 'label': 'Media Location', 'width': '200px'},
+        {'key': 'delete', 'label': 'Delete', 'width': '80px'},
+        {'key': 'update', 'label': 'Update', 'width': '80px'},
+    ]
+
+    # get the movies data that have the specific genre
+    db = get_db()
+    genre = db.execute('SELECT * from genre where id = ?', (genre_id,)).fetchone()
+    movies = db.execute(
+        'SELECT m.* '
+        'FROM movie AS m '
+        'JOIN movie_genre AS mg ON m.id = mg.movie_id '
+        'WHERE mg.genre_id = ?', (genre_id,)
+    ).fetchall()
+
+    # no movies shown
+    style = "display: none;"
+    if (len(movies) <= 0):
+        style = "display: block;"
+
+    return render_template("admin/genre.jinja2",
+                           genre=genre,
+                           movies=movies,
+                           table_columns=table_columns,
+                           style=style,)
+
+
+
+@bp.route('/delete-genre/<string:genre_id>', methods=["POST"])
+@admin_required
+def delete_genre(genre_id: str):
+    # returns a 404 if genre_id does not exist
+    get_genre(genre_id)
+
+    # delete the genre from database
+    db = get_db()
+    db.execute('DELETE FROM genre WHERE id = ?', (genre_id,))
+    db.commit()
+
+    return redirect(url_for('admin.movies'))
+
+
+
 @bp.route('/edit-genre', methods=["GET", "POST"])
 @admin_required
 def edit_genre():
@@ -473,3 +413,65 @@ def get_genre(genre_id: str) -> Any:
         abort(404, f"Genre ID {genre_id} does not exist.")
     
     return genre
+
+def _validate_movie_entries(movie_data: dict[str, Any]) -> str:
+    
+    # initialise the variable 'error' (to be returned)
+    error = ""
+    
+    # get the database
+    db = get_db()
+    
+    # get the movie data
+    movie_name = movie_data['movieName']
+    movie_description = movie_data['movieDescription']
+    imdb_rating = movie_data['imdbRating']
+    rotten_tomatoes_rating = movie_data['rottenTomatoesRating']
+    metacritic_rating = movie_data['metacriticRating']
+    release_date = movie_data['releaseDate']
+    media_location = movie_data['mediaLocation']
+    poster_location = movie_data['posterLocation']
+    duration = movie_data['duration']
+    
+    # validation 1: the below data are all required
+    if not movie_name:
+        error = "Movie Name is required."
+    elif not movie_description:
+        error = "Movie Description is required."
+    elif not release_date:
+        error = "Release Date is required."
+    elif not media_location:
+        error = "Media Location is required."
+    elif not poster_location:
+        error = "Poster Location is required."
+
+    # validation 2: no two movie names exists
+    name = db.execute('SELECT name FROM movie WHERE name = ?', (movie_name,)).fetchone()
+    if (name is not None):
+        error = "Movie name already exists."
+    
+    # validation 3: no two media locations is the same
+    loc = db.execute('SELECT media_location FROM movie WHERE media_location = ?', (media_location,)).fetchone()
+    if (loc is not None):
+        error = "Media location already exists. New movie cannot be from the same existing media location."
+
+    # validation 3: both metacritic rating and rotten tomatoes rating need to be from 0 to 100
+    pattern = r'^(100|[1-9]?[0-9])$'
+    if (rotten_tomatoes_rating):
+        if not bool(re.match(pattern, rotten_tomatoes_rating)):
+            error = "Rotten Tomatoes Rating should be from 0 to 100."
+    if (metacritic_rating):
+        if not bool(re.match(pattern, metacritic_rating)):
+            error = "Metacritic Rating should be from 0 to 100."
+
+    # validation 4: imdb rating needs to be from 0.0 to 10.0
+    pattern = r'^(10\.0|[0-9]\.[0-9])$'
+    if (imdb_rating):
+        if not bool(re.match(pattern, imdb_rating)):
+            error = "IMDb Rating should be from 0.0 to 10.0 (strictly only 1 decimal place)."
+
+    if (duration):
+        if (not duration.isnumeric()):
+            error = "Duration needs to be a number"
+
+    return error
